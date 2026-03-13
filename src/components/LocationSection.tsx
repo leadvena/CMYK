748e8ee86e7a14974b8d57bb69b97a637c1eb476
leadvena <leadvena08@gmail.com>
2026-03-13 10:33:02 +0800

    chore: updated quote form to send design file and map iframe

--- src/components/LocationSection.tsx
+++ src/components/LocationSection.tsx
@@ -30,7 +30,7 @@ const LocationSection = () => {
         >
           <iframe
             title="CMYK Prints Location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3920.0!2d124.0!3d11.04!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTHCsDAyJzI0LjAiTiAxMjTCsDAwJzAwLjAiRQ!5e0!3m2!1sen!2sph!4v1710000000000"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3919.0!2d124.00506603153785!3d11.05201895488637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1710000000000"
             width="100%"
             height="450"
             style={{ border: 0 }}
